4dc98d24c92829245c77ab3b6038cff0800778b9
Jacquelinne Marcano <jacquelinnemarcano@gmail.com>
2025-02-24 12:02:07 -0400

    Update ui.py
    Moved the styling to MS.TLabel style. Made mainframe the parent for these components, as it's literally the main frame. Might edit this to have two frames inside mainframe, one for the UI elements of timer and flags, and whatever else, and one for the gameboard.

--- ui.py
+++ ui.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import ttk
 from utils import Timer
 from PIL import Image, ImageTk
+from styles import MinesweeperStyles
 
 class UI:
     root = Tk()
@@ -12,23 +13,23 @@ class UI:
         :param game: a Minesweeper game
         """
         self.game = game
+        self.styles = MinesweeperStyles()
+        self.mainframe = ttk.Frame(self.root, width=2, height=8, padding="3 3 12 12", style="MS.TFrame" )
+        self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
 
-        self.mainframe = ttk.Frame(self.root, padding="3 3 12 12")
-        
         self.root.title("Minesweeper")
         self.buttons = []
-        self.bg_color = "#999999"
-        self.bg_color_selected = "#656565"
-        self.bg_color_bomb = "red"
         self.timer = Timer()
-        self.time_label = ttk.Label(master=self.root, text="Time: 0.0", font=("Helvetica", 8))
+        self.time_label = ttk.Label(master= self.mainframe, text="Time: 0.0", style="MS.TLabel")
         self.time_label.grid(row=0, column=0, columnspan=self.game.width,sticky="w")
-        self.mines_marked_label = ttk.Label(master=self.root, text="Flags: 0/{}".format(self.game.num_mines), font=("Helvetica", 8))
+        self.mines_marked_label = ttk.Label(master=self.mainframe, text="Flags: 0/{}".format(self.game.num_mines), style="MS.TLabel")
         self.mines_marked_label.grid(row=0, column=self.game.width//2, columnspan=self.game.width//2, sticky="e")
         for y in range(self.game.height):
             row = []
             for x in range(self.game.width):
-                button = ttk.Button(self.root, text="", width=2, command=lambda x=x, y=y: self.click(x, y))
+                button = ttk.Button(self.mainframe, text="", command=lambda x=x, y=y: self.click(x, y), style="MS.TButton")
                 button.grid(row=y+1, column=x, sticky="nsew")
                 button.bind("<3>", lambda event, x=x, y=y: self.right_click(x, y))
                 row.append(button)
@@ -36,16 +37,16 @@ class UI:
 
     def update_timer(self):
         """
-        Update the timer label every 100ms to display the current elapsed time.
+        Update the timer label every second to display the current elapsed time.
         
         This method is called repeatedly by the after method to update the timer
         label. It stops once the game is over.
         """
         
         self.timer.update()
-        time_str = "Time: {:.1f}".format(self.timer.get_time())
+        time_str = "Time: {:.0f}".format(self.timer.get_time())
         self.time_label.config(text=time_str)
-        self.timer_id = self.root.after(100, self.update_timer)  # store the ID
+        self.timer_id = self.root.after(1000, self.update_timer)  # store the ID
 
     def click(self, x, y):
         if self.game.game_over or self.game.game_win:
@@ -57,20 +58,29 @@ class UI:
         if self.game.grid[y][x] == -1:
             bomb_image = ImageTk.PhotoImage(Image.open("assets/bomb.png"))
             # self.buttons[y][x].config(text="*", bg="red")
-            self.buttons[y][x].config(image=bomb_image)
+            self.buttons[y][x].config(image=bomb_image, style="MS_bomb.TButton")
             self.buttons[y][x].image = bomb_image  # keep a reference to the image
             self.game.game_over = True
             self.root.after_cancel(self.timer_id)  # stop the timer
+
+        # Get all bombs and show them, unless the bomb has been flagged.
+            for i in range(self.game.height):
+                for j in range(self.game.width):
+                    if self.game.grid[i][j] == -1 and not self.game.revealed[i][j]:
+                        if not hasattr(self.buttons[i][j], 'flagged'):
+                            bomb_image = ImageTk.PhotoImage(Image.open("assets/bomb.png"))
+                            self.buttons[i][j].config(image=bomb_image)
+                            self.buttons[i][j].image = bomb_image  # keep a reference to the image
         # Else if tile is not a bomb, and is empty:
         elif self.game.grid[y][x] == 0:
-            self.buttons[y][x].config(text="")
+            self.buttons[y][x].config(text="", state="disabled",  style="MS_pressed.TButton")
             for dy in [-1, 0, 1]:
                 for dx in [-1, 0, 1]:
                     if 0 <= x+dx < self.game.width and 0 <= y+dy < self.game.height:
                         self.click(x+dx, y+dy)
         # Else show the number of the tile:
         else:
-            self.buttons[y][x].config(text=str(self.game.grid[y][x]))
+            self.buttons[y][x].config(text=str(self.game.grid[y][x]), state="disabled", style="MS_pressed.TButton")
         
         if self.game.game_over:
             self.game_over()
@@ -79,7 +89,7 @@ class UI:
             self.game_win()
 
     def right_click(self, x, y):
-        if self.game.revealed[y][x]:
+        if self.game.revealed[y][x] or self.game.game_win or self.game.game_over:
             return
         if hasattr(self.buttons[y][x], 'flagged'):
             # Remove flag
@@ -88,7 +98,7 @@ class UI:
         else:
             # Add flag
             flag_image = ImageTk.PhotoImage(Image.open("assets/bomb-flag.png"))
-            self.buttons[y][x].config(image=flag_image, )
+            self.buttons[y][x].config(image=flag_image)
             self.buttons[y][x].image = flag_image  # keep a reference to the image
             self.buttons[y][x].flagged = True
 
