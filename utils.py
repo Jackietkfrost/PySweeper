998685b3608bcaed555a0c3c325bf20c530bf4cc
Jacquelinne Marcano <jacquelinnemarcano@gmail.com>
2025-02-25 10:48:07 -0400

    Update utils.py
    Updated the timer to start when tile is clicked instead of when the program was opened.

--- utils.py
+++ utils.py
@@ -2,10 +2,12 @@ import time
 
 class Timer:
     def __init__(self):
-        self.start_time = time.time()
+        self.start_time = None
         self.elapsed_time = 0
 
     def update(self):
+        if self.start_time is None:
+            self.start_time = time.time()
         self.elapsed_time = time.time() - self.start_time
 
     def get_time(self):
